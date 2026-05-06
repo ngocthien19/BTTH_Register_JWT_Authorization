1eaacee0a7ba47512e361fc91161a13feb33afe2
toantran00 <toan008888@gmail.com>
2026-05-06 16:31:03 +0700

    complete loginJWT and authorization

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const verifyController = require('../controllers/verifyController')
-const { registerRateLimiter, validateRegisterInput } = require('../middleware/registerMiddleware')
 const registerController = require('../controllers/registerController')
 const forgotPasswordController = require('../controllers/forgotPasswordController')
+const authController = require('../controllers/authController')
+// const userController = require('../controllers/userController')
+// const { protect } = require('../middleware/authMiddleware')
+const { registerRateLimiter, validateRegisterInput } = require('../middleware/registerMiddleware')
+const { loginRateLimiter, validateLoginInput } = require('../middleware/loginMiddleware')
 
 router.post('/register', 
     registerRateLimiter,      
@@ -17,4 +21,11 @@ router.post('/forgot-password', forgotPasswordController.handleForgotPassword)
 
 router.post('/reset-password', forgotPasswordController.handleResetPassword)
 
+router.post('/login', 
+  loginRateLimiter, 
+  validateLoginInput, 
+  authController.handleLogin
+)
+
+// router.put('/edit-profile', protect, userController.handleEditProfile)
 module.exports = router
