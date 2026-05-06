2b39b3a4dea85a3785e07344f9d4a109e4d28c76
Cao Ngoc Thien <caongocthien1902@gmail.com>
2026-05-06 12:25:49 +0700

    complete forgot_password feature

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const verifyController = require('../controllers/verifyController')
 const { registerRateLimiter, validateRegisterInput } = require('../middleware/registerMiddleware')
 const registerController = require('../controllers/registerController')
+const forgotPasswordController = require('../controllers/forgotPasswordController')
 
 router.post('/register', 
     registerRateLimiter,      
@@ -11,5 +12,9 @@ router.post('/register',
 )
 
 router.post('/verify-otp', verifyController.handleVerifyOTP)
+
+router.post('/forgot-password', forgotPasswordController.handleForgotPassword)
+
+router.post('/reset-password', forgotPasswordController.handleResetPassword)
 
 module.exports = router
